6f8500341676146ce7609a87df1fa7f0fcd1e6df
Huseyin Teoman Deniz <54050305+TeomanDeniz@users.noreply.github.com>
2022-12-29 12:07:38 +0300

    Update ft_pow.c

--- ft_functions/ft_pow/ft_pow.c
+++ ft_functions/ft_pow/ft_pow.c
@@ -13,8 +13,8 @@
 long long
 	ft_pow(register long long base, register signed int power)
 {
-	register volatile signed int		i;
-	register volatile long long	x;
+	register volatile signed int	i;
+	register volatile long long		x;
 
 	i = 1;
 	x = base;
