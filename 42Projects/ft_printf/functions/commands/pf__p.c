8c3c4286cd720ae8c76e5fe5302b6a2118e271c3
Huseyin Teoman Deniz <54050305+TeomanDeniz@users.noreply.github.com>
2023-01-22 11:32:02 +0300

    Update pf__p.c

--- 42Projects/ft_printf/functions/commands/pf__p.c
+++ 42Projects/ft_printf/functions/commands/pf__p.c
@@ -16,6 +16,6 @@ void
 	pf__p(unsigned long long address, int *flags)
 {
 	pf_flag_event(flags, 'p', (void *)&address, 'a');
-	flags[1] += ft_itoa_base(address, 16, 'x');
+	flags[1] += ft_itoa_base(address, 16, 'x', flags[0]);
 	pf_flag_event(flags, 'p', (void *)&address, 'b');
 }
